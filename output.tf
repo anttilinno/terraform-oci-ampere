572cfecc3cdca0f6ea46222f0b2d5db3bdaace37
Antti <antti.linno@gmail.com>
2022-02-20 22:14:09 +0200

    Add working terraform for Oracle OCI

---
+++ output.tf
@@ -0,0 +1,19 @@
+# Output the private and public IPs of the instance
+
+output "AmpereA1_PrivateIPs" {
+  value = ["${oci_core_instance.ampere_a1.*.private_ip}"]
+}
+
+output "AmpereA1_PublicIPs" {
+  value = ["${oci_core_instance.ampere_a1.*.public_ip}"]
+}
+
+# Output the boot volume IDs of the instance
+output "AmpereA1_BootVolumeIDs" {
+  value = ["${oci_core_instance.ampere_a1.*.boot_volume_id}"]
+}
+
+# Output development compartment ID
+output "Development_CompartmentID" {
+  value = "oci_identity_compartment.tf-compartment.id"
+}
